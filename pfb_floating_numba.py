FIR filter coefficient and scaling fixed. Optimised pfb_floating.py and fixed.py. Have a matched between the CASPER PFB and simulator for 4k, 8-tap hanning.
FIR filter now generates coefficients in the same fashion as the pfb_coeff_gen script in mlib_devel and applies a scaling factor in the same way.

All scripts have been edited to remove/add features that were necessary/unecessary.

pfb_floating_numba.py is somewhat optimised with numba and runs a lot faster than pfb_floating.py.

This is the first time I've had a smack-bang match between simulator and CASPER PFB for a 4k, 8 tap, hanning PFB.

If I were more confident I might consider this version 1.0.

pfb_floating_numba.py
@@ -5,28 +5,36 @@ Created on Thu Aug 16 15:21:43 2018
 @author: talonmyburgh
 """
 import numpy as np
+import numba as nb
 from pfb_coeff_gen import coeff_gen
 
 # =============================================================================
-# Bit reversal algorithms used for the iterative fft's data re-ordering
+# Bit reversal algorithms used for the iterative fft's
 # =============================================================================
 """Arranges chronological values in an array in a bit reversed fashion"""
+@nb.jit(nopython=True)
 def bit_rev(a, bits):
     a_copy = a.copy()
+    sz = a.shape[0]
     N = 1<<bits
     for i in range(1,bits):
         a >>=1
         a_copy <<=1
-        a_copy |= (a[:]&1)
-    a_copy[:] &= N-1
+        for j in range(sz):    
+            a_copy[j] |= (a[j]&1)
+    for k in range(sz):       
+        a_copy[k] &= N-1
     return a_copy
 
 """Takes an array of length N which must be a power of two"""
+@nb.jit(nopython=False)
 def bitrevarray(array,N): 
     bits = int(np.log2(N))                                                     #number of bits to repr numbers in array
     A = np.empty(N,dtype=np.complex64)
-    a=np.arange(N)
-    A[bit_rev(a,bits)] = array[:]
+    a=np.arange(N,dtype = np.int64)
+    revind = bit_rev(a,bits)
+    for i in nb.prange(N):
+        A[revind[i]] = array[i]
     return A
 
 # =============================================================================
@@ -34,20 +42,23 @@ def bitrevarray(array,N):
 # order out.
 # =============================================================================
 """Generate array of needed twiddles"""
-def make_twiddle(N):                                                           
+@nb.jit(nopython=True)
+def make_twiddle(N):                                                           #generate array of needed twiddles
     i=np.arange(N//2)
     arr = np.exp(-2*i*np.pi*1j/N)
     return arr
 
 """Natural order in DIT FFT that accepts the data, the twiddle factors
 (bit reversed) and allows for staging"""
+@nb.jit(nopython=False)
 def iterfft_natural_in_DIT(DATA,twid,staged=False):
     data = np.asarray(DATA,dtype = np.complex64)
     N = data.shape[0]                                                          #how long is data stream
+    stgd_data = np.zeros((N,int(np.log2(N))+2),dtype = np.complex64)
     
     if(staged):
-        stgd_data = np.zeros((N,int(np.log2(N))+2),dtype = np.complex64)
-        stgd_data[:,0] = data[:]
+        for i in range(N):
+            stgd_data[i,0] = data[i]
     num_of_groups = 1                                                          #number of groups - how many subarrays are there?
     distance = N//2                                                            #how far between each fft arm?
     stg=1                                                                      #stage counter
@@ -55,21 +66,23 @@ def iterfft_natural_in_DIT(DATA,twid,staged=False):
     while num_of_groups < N:                                                   #basically iterates through stages
         for k in range(num_of_groups):                                         #iterate through each subarray
             jfirst = 2*k*distance                                              #index to beginning of a group
-            jlast = jfirst + distance - 1                                      #first index plus offset - used to index whole group
+            jlast = jfirst + distance                                      #first index plus offset - used to index whole group
             W=twid[k]
-            slc1 = slice(jfirst,jlast+1,1)
-            slc2 = slice(jfirst+distance, jlast+1+distance,1)
-            tmp = W*data[slc2]
-            data[slc2] = data[slc1]-tmp
-            data[slc1] = data[slc1]+tmp
+            for j in range(jfirst,jlast):
+                jdist = j+distance
+                tmp = W*data[jdist]
+                data[jdist] = data[j]-tmp
+                data[j] = data[j]+tmp
         num_of_groups *=2
         distance //=2
         if(staged):                                                            #if we are recording stages
-            stgd_data[:,stg]=data[:]                                           #log each stage data to array
+            for l in range(N):
+                stgd_data[l,stg]=data[l]                                       #log each stage data to array
         stg+=1
         
-    if(staged): 
-        stgd_data[:,-1] = bitrevarray(stgd_data[:,-2],N)                       #post bit-reordering for last stage - added as extra stage
+    if(staged):
+        for m in range(N):
+            stgd_data[m,-1] = bitrevarray(stgd_data[m,-2],N)                   #post bit-reordering for last stage - added as extra stage
         return stgd_data
     else:
         A=bitrevarray(data,N)                                                  #post bit-reordering
@@ -77,15 +90,14 @@ def iterfft_natural_in_DIT(DATA,twid,staged=False):
 
 # =============================================================================
 # Floating point PFB implementation making use of the natural order in fft
-# like CASPER does. 
+# like SARAO does. 
 # =============================================================================
-
 class FloatPFB(object):
         """This function takes point size, how many taps, what percentage of total data to average over,
         to get data from a file or not,what windowing function, whether you're running dual polarisations,
         whether you'd like data from a stage, and if so which stage - stage 0 being the data in"""
         def __init__(self, N, taps, avg = 1, datasrc = None, w = 'hann',dual = False,
-                     staged = False, fwidth=1, chan_acc = False):
+                     staged = False, fwidth=1, chan_acc =False):
             self.N = N                                                         #how many points
             self.avg = avg                                                     #what averaging
             self.dual = dual                                                   #whether you're performing dual polarisations or not
@@ -94,29 +106,30 @@ class FloatPFB(object):
             self.staged=staged
             self.fwidth = fwidth
             self.chan_acc = chan_acc
-
+                
             if(datasrc is not None and type(datasrc)==str):                    #if input data file is specified
                 self.inputdatadir = datasrc
                 self.outputdatadir = datasrc[:-4]+"out.npy"
                 self.inputdata  = np.load(datasrc, mmap_mode = 'r')
             else:
                 self.inputdata = None
-            
-            self.window,self.firsc=coeff_gen(N,taps,w,self.fwidth)             #Get window coefficients and scaling 
-                                                                               #factor to use in FIR registers.
+                
+            self.window,self.firsc=coeff_gen(N,taps,w,self.fwidth)
+                
             self.twids = make_twiddle(self.N)
             self.twids = bitrevarray(self.twids, len(self.twids))              #for natural order in FFT
                 
         """Takes data segment (N long) and appends each value to each fir.
-        Returns data segment (N long) that is the sum of fircontents*windowcoeffs"""
+        Returns data segment (N long) that is the sum of fircontents*window"""
         def _FIR(self,x):
-            X = np.sum(self.reg*self.window,axis=1) / (2**self.firsc)          #filter and scale
+            X = np.sum(self.reg*self.window,axis=1) / (2**self.firsc)          #filter
             self.reg = np.column_stack((x,self.reg))[:,:-1]                    #push and pop from FIR register array
             return X
         
-        """For dual polarisation processing, we need to split the data after
-        FFT and return the individual complex spectra"""        
+        """In the event that that dual polarisations have been selected, we need to 
+        split out the data after and return the individual X_k values"""        
         def _split(self,Y_k):
+            #reverse the arrays for the splitting function correctly
             R_k = np.real(Y_k)
             R_kflip = R_k.copy()
             R_kflip[1:] = R_kflip[:0:-1]
@@ -128,20 +141,19 @@ class FloatPFB(object):
             self.G_k = (1/2)*(R_k+1j*I_k+R_kflip-1j*I_kflip)                   #declares two variables for 2 pols
             self.H_k = (1/2j)*(R_k+1j*I_k-R_kflip+1j*I_kflip)
         
-        
         """Here we take the power spectrum of the outputs. Chan_acc dictates
-        if one must sum over all outputs produced."""       
+        if one must sum over all outputs produced."""        
         def _pow(self,X):
             if (self.chan_acc):                                                #if accumulation specified
                 pwr = X * np.conj(X)
-                pwr = np.real(np.sum(pwr,axis=1))
+                pwr = np.sum(pwr,axis=1)
                 return pwr
             else:                                                              #if no accumulation specified
-                pwr = np.real((X * np.conj(X)))
+                pwr = X * np.conj(X)
                 return pwr
                         
-        """Here one parses a data vector to the PFB to run. Note it must be
-        numpy array of length N if a data file was not specified before"""
+        """Given data, (having specified whether the PFB will run in dual or not)
+        you parse the data and the PFB will compute the spectrum"""
         def run(self,data=None):
             
             if (data is not None):                                             #if we are using an input data array
@@ -152,9 +164,9 @@ class FloatPFB(object):
             size = self.inputdata.size                                         #get length of data stream
             stages = size//self.N                                              #how many cycles of commutator
             
-            if(self.staged):                                                   #if storing staged data
-                X = np.empty((self.N,stages,int(np.log2(self.N))+2),
-                             dtype = np.complex64)
+            if(self.staged):                                                   #if recording staged data
+                X = np.zeros((self.N,stages,int(np.log2(self.N))+2),
+                             dtype = np.complex64)  
                                                                                #will be tapsize x datalen/point x stages
                 for i in range(0,stages):                                      #for each stage, populate all firs, and run FFT once
                     if(i ==0):
@@ -166,18 +178,22 @@ class FloatPFB(object):
                                 self.inputdata[i*self.N-1:i*self.N+self.N-1]),self.twids,
                             self.staged)
                 
-            else:                                                              #if storing staged data
-                X = np.empty((self.N,stages),dtype = np.complex64)
+            else:                                                              #if not recording staged data
+                X = np.zeros((self.N,stages),dtype = np.complex64)
+                XFIR = X.copy()
                                                                                #will be tapsize x stages
                 for i in range(0,stages):                                      #for each stage, populate all firs, and run FFT once
                     if(i ==0):
                         X[:,i] = iterfft_natural_in_DIT(self._FIR(
-                                self.inputdata[0:self.N]),
+                                self.inputdata[i*self.N:i*self.N+self.N]),
                             self.twids)
+                        XFIR[:,i] = self._FIR(self.inputdata[0:self.N])
                     else:
                         X[:,i] = iterfft_natural_in_DIT(self._FIR(
                                 self.inputdata[i*self.N-1:i*self.N+self.N-1]),
                             self.twids)
+                        XFIR[:,i] = self._FIR(self.inputdata[i*self.N-1:i*self.N+self.N-1])
+                        
             
             """Decide on how to manipulate and display output data"""
             if(self.dual and not self.staged):                                 #If dual processing but not staged                      

singletoneandnoise.py
@@ -1,48 +1,111 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 29 20:03:43 2019
+Created on Wed Jun 19 21:15:39 2019
 
 @author: talon
 """
 
 import numpy as np
 
-iters = 1e2
-N = int(2**16*iters)
-noise = ((np.random.normal(size=N,scale = 0.3)).astype(np.float64)/15000)
-tone = ((np.cos(7000*2*np.pi*np.arange(N)/N).astype(np.float64)+0.5)/20)
-input_vector = noise+tone
-
-from fixpoint import fixpoint, cfixpoint
+from fixpoint import cfixpoint,fixpoint
 from pfb_fixed import iterffft_natural_DIT as fixFFT
-from pfb_fixed import make_fix_twiddle, bitrevfixarray, bit_rev
+from pfb_fixed import make_fix_twiddle, bitrevfixarray
+from pfb_floating_numba import iterfft_natural_in_DIT as floatFFT
+from pfb_floating_numba import bitrevarray, make_twiddle
 
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('%s |%s| %s%% %s' % (prefix, bar, percent, suffix),end="\r")
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
-#RUN 22 bit twiddle
-fixinput = cfixpoint(22,22) #22 bit fixpoint number that will use even-rounding
-fixtwids18 = make_fix_twiddle(2**16,18,17) #18 bit twiddle factor that uses even rounding
-fixtwids18 = bitrevfixarray(fixtwids18,fixtwids18.data.size)
+N=2**13
+accumval = 500
+dmpnum = 0
 
-fixtwids22 = make_fix_twiddle(2**16,22,21) #22 bit twiddle factor that uses even rounding
-fixtwids22 = bitrevfixarray(fixtwids22,fixtwids22.data.size)
+fixinputev = cfixpoint(17,17,method = "ROUND") #18bit using even rounding
+fixtwidsev = make_fix_twiddle(N,17,16,method="ROUND") #18 bit twiddle factor that uses even rounding
+fixtwidsev = bitrevfixarray(fixtwidsev,fixtwidsev.data.size)
 
-shiftreg = [1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1]
-output_vector18 = np.zeros(2**15,dtype=np.float64)
-output_vector22 = np.zeros(2**15,dtype=np.float64)
+fixinputinf = cfixpoint(17,17,method = "ROUND_INFTY") #18bit using infty rounding
+fixtwidsinf = make_fix_twiddle(N,17,16,method="ROUND_INFTY") #18 bit twiddle factor that uses infty rounding
+fixtwidsinf = bitrevfixarray(fixtwidsinf,fixtwidsinf.data.size)
 
+floattwids = make_twiddle(N)
+floattwids = bitrevarray(floattwids,floattwids.size)
 
+output_vectorev = fixpoint(31,31,method="ROUND")
+output_vectorinf = fixpoint(31,31,method="ROUND")
 
-for i in range(iters):
-    print(i)
-    slc = slice(int(i*2**16),int(i*2**16+2**16),1)
-    fixinput.from_complex(input_vector[slc])
-    out18= np.abs(fixFFT(fixinput,fixtwids18,shiftreg.copy(),22,22,18,staged=False).to_complex())[:2**15]
-    output_vector18 += out18
-    out22= np.abs(fixFFT(fixinput,fixtwids22,shiftreg.copy(),22,22,22,staged=False).to_complex())[:2**15]
-    output_vector22 += out22
+shiftreg = [1,1,1,1,1,1,0,1,1,1,1,1,1]
 
-np.save("output_vector_18bit_twiddle",output_vector18)
+i=0
+arnge = np.arange(N)
 
-np.save("output_vector_22bit_twiddle",output_vector22)
+while(True):
+    if (i==0):
+        printProgressBar(i, accumval, prefix = 'Progress through integration '+str(dmpnum)+':', suffix = 'Complete', length = 50)
+        output_vectorev.from_float(np.zeros(2**12,dtype=np.float64))
+        output_vectorinf.from_float(np.zeros(2**12,dtype=np.float64))
+        output_float = np.zeros(2**12,dtype=np.float64)
+    tn = np.sin(280*np.pi*arnge/N,dtype=np.float64)/20
+    nse = (np.random.normal(size=N,scale = 0.3)/800).astype(np.float64)
+    input_vector = tn+nse
+    fixinputev.from_complex(input_vector)
+    fixinputinf.from_complex(input_vector)
+    
+    fftoutev = fixFFT(fixinputev,fixtwidsev,shiftreg.copy(),17,17,17,staged=False)[:2**12]
+    outev = fftoutev.power()
+    outev.data = outev.data*2**10
+    outev.bits=31
+    outev.fraction=31
+    outev.normalise()
+    
+    output_vectorev = output_vectorev+outev
+    output_vectorev.bits=31
+    output_vectorev.fraction=31
+    output_vectorev.normalise()
+    
+    fftoutinf = fixFFT(fixinputinf,fixtwidsinf,shiftreg.copy(),17,17,17,staged=False)[:2**12]
+    outinf = fftoutinf.power()
+    outinf.data = outinf.data*2**10
+    outinf.bits=31
+    outinf.fraction=31
+    outinf.normalise()
+    
+    output_vectorinf = output_vectorinf+outinf
+    output_vectorinf.bits=31
+    output_vectorinf.fraction=31
+    output_vectorinf.normalise()
+    
+    outflt = floatFFT(input_vector,floattwids,staged=False)[:2**12]
+    output_float += (outflt*np.conj(outflt)).astype(np.float64)
+    
+    if (i==accumval):
+        np.save("output_vector_evenrounding"+str(dmpnum),output_vectorev.data)
+        np.save("output_vector_infiniterounding"+str(dmpnum),output_vectorinf.data)
+        np.save("output_vector_float"+str(dmpnum),output_float)
+        dmpnum+=1
+        i=0
+        print("Integration "+str(dmpnum)+" complete!")
+    else:
+        i+=1
+        printProgressBar(i, accumval, prefix = 'Progress through integration '+str(dmpnum)+':', suffix = 'Complete', length = 50)
+   

timingtests.py
@@ -7,14 +7,43 @@ Created on Sun Jun  2 13:16:52 2019
 """
 
 from timeit import timeit as t
-
-upset = '''from fixpoint import fixpoint
 import numba as nb
 import numpy as np
-from pfb_fixed import bitrevfixarray
-fp = fixpoint(10,10)
-fp.from_float(np.arange(1024))'''
 
-code = '''bitrevfixarray(fp,1024)'''
+@nb.jit(nopython = True)
+def roundinfty(array):
+    a = array.copy()
+    f=np.modf(a)[0]                                                        #get decimal values from data
+    if (a.ndim == 1):                                                      #for 1D array
+        for i in range(len(array)):
+            if((f[i]<0.0 and f[i] <=-0.5) or (f[i]>=0.0 and f[i]<0.5)):
+                a[i]=np.floor(a[i])
+            else:
+                a[i]=np.ceil(a[i])
+    elif(a.ndim==2):                                                       #for 2D array
+        for i in range(array.shape[0]):
+            for j in range(array.shape[1]):
+                if((f[i,j]<0.0 and f[i,j] <=-0.5) or (f[i,j]>=0.0
+                   and f[i,j]<0.5)):
+                    a[i,j]=np.floor(a[i,j])
+                else:
+                    a[i,j]=np.ceil(a[i,j])
+    elif(a.ndim==3):                                                       #for 3D array
+        for i in range(array.shape[0]):
+            for j in range(array.shape[1]):
+                for k in range(array.shape[2]):
+                    if((f[i,j,k]<0.0 and f[i,j,k] <=-0.5) or 
+                       (f[i,j,k]>=0.0 and f[i,j,k]<0.5)):
+                        a[i,j,k]=np.floor(a[i,j,k])
+                    else:
+                        a[i,j,k]=np.ceil(a[i,j,k])
+    return a
 
-print(t(stmt=code,setup=upset,number=10000))
+upset = '''from pfb_floating_numba import FloatPFB
+import numpy as np
+data = np.ones(8*2**13)
+pfbflt = FloatPFB(2**13,8, chan_acc = True)'''
+
+code = '''pfbflt.run(data)'''
+
+print(t(stmt=code,setup=upset,number=1000))