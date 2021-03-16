850e7730a4f80ac877041b2e4c5146b2abb99a33
talonmyburgh <myburgh.talon@gmail.com>
2021-03-16 11:27:10 +0200

    Created coef gen

---
+++ pfb_coeff_gen.jl
@@ -0,0 +1,25 @@
+# Binary Output
+function coeff_gen(N :: Integer, taps :: Integer; win :: String = "hanning", fwidth :: Float64 = 1.0) :: {Array{<:Real},Integer}
+    WinDic = Dict{String,Function}(                                                                     #dictionary of various filter types
+    "hanning" => DSP.hanning,
+    "hamming" => DSP.hamming,
+    "bartlett" => DSP.bartlett,
+    "blackman" => DSP.blackman,
+    );
+    alltaps = N*taps;
+    windowval=WinDic[win](alltaps);                                               
+    totalcoeffs = transpose(windowval.*sinc.(fwidth.*(collect(0:alltaps-1)./(N) .- taps/2)));
+    scalefac = nextpow2(max(sum(abs(totalcoeffs),dims=1)));
+    return totalcoeffs, scalefac;
+end
+
+function nextpow2(val::Float64) :: Integer
+    i = 0;
+    while true
+        if 2^i >= val
+            return i;
+        else
+            i+=1;
+        end
+    end
+end
